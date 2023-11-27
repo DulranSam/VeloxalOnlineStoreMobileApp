ef058e6146a10c3ff4d85cde17d0592e08ae9dad
Veloxal <dulransamarasinghe4@gmail.com>
2023-11-27 19:21:29 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -17,81 +17,126 @@ class MainApp extends StatelessWidget {
           title: const Text("Veloxal"),
         ),
         body: Center(
-          child: SizedBox(
-            height: double.infinity,
-            width: double.infinity,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.network(
-                    "https://us-tuna-sounds-images.voicemod.net/58cdc2c9-fcc3-4d4f-a60f-52b024b3a5ba-1671409307595.jpg",
-                    cacheHeight: 300,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Welcome to Veloxal ",
+                  style: TextStyle(fontSize: 32),
+                ),
+                Image.network(
+                  "https://img.freepik.com/premium-vector/cute-orange-cat-waving-paw-cartoon-vector-illustration_42750-818.jpg?w=2000",
+                  scale: 10,
+                ),
+                const Text(
+                  "Login",
+                  style: TextStyle(fontSize: 32, color: Colors.black),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter your username',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your username';
+                      }
+                      return null;
+                    },
                   ),
-                  const Text(
-                    "Login",
-                    style: TextStyle(fontSize: 32, color: Colors.black),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter your Password',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      return null;
+                    },
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 4,
-                      vertical: 4,
-                    ),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: 'Enter your username',
-                      ),
-                    ),
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.black),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 4,
-                      vertical: 4,
-                    ),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: 'Enter your Password',
-                      ),
-                    ),
-                  ),
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.black),
-                    ),
-                    onPressed: () {
+                  onPressed: () {
+                    if (Form.of(context)!.validate()) {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => PlaceholderScreen(),
+                          builder: (context) => const SecondRoute(),
                         ),
                       );
-                    },
-                    child: const Text(
-                      "Login",
-                      style: TextStyle(fontSize: 24),
-                    ),
-                  )
-                ],
-              ),
+                    }
+                  },
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Text("Not a user yet? "),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.blue),
+                        ),
+                        child: const Text(
+                          "Click Here to Register",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.wifi), label: "Wifi"),
+          ],
         ),
       ),
     );
   }
 }
 
-class PlaceholderScreen extends StatelessWidget {
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Welcome Screen"),
+        title: const Text('Second Route'),
       ),
-      body: const Center(
-        child: Text("Welcome! This is the destination screen."),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
       ),
     );
   }
